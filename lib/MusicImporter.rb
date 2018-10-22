c5f93cf547985b2539a5d9e010308f8d2e1ebefe
Austin  Burke <aburk3@gmail.com>
2018-10-22 16:50:25 +0000

    Done.

--- lib/MusicImporter.rb
+++ lib/MusicImporter.rb
@@ -1,3 +1,19 @@
+require 'pry'
+
 class MusicImporter
+  attr_accessor :path
+
+  def initialize(file_path)
+    @path = file_path
+  end
+
+  def files
+    @files ||= Dir.glob("#{path}/*.mp3").collect do |file|
+      file.gsub("#{path}/", "")
+    end
+  end
+
+  def import
+  end
 
 end
