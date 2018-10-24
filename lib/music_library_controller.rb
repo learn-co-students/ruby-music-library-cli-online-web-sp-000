e038e942d39a008bbb5b3ffdaece938e69d92a7d
Austin  Burke <aburk3@gmail.com>
2018-10-24 17:11:38 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -9,9 +9,9 @@ class MusicLibraryController
   end
 
   def call
-    name = ""
+    user_input = ""
 
-    while name != "exit"
+    while user_input != "exit"
       puts "Welcome to your music library!"
       puts "To list all of your songs, enter 'list songs'."
       puts "To list all of the artists in your library, enter 'list artists'."
@@ -22,8 +22,22 @@ class MusicLibraryController
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
 
-      name = gets
-      name = gets.chomp
+      user_input = gets.chomp
+
+      case user_input
+      when "list songs"
+        list_songs
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres
+      when "list artist"
+        list_songs_by_artist
+      when "list genre"
+        list_songs_by_genre
+      when "play song"
+        play_song
+      end
     end
   end
 
@@ -57,6 +71,29 @@ class MusicLibraryController
     if artist = Artist.find_by_name(user_input)
       artist.songs.sort{ |instance1, instance2| instance1.name <=> instance2.name }.each.with_index(1) do |instance, index|
           puts "#{index}. #{instance.name} - #{instance.genre.name}"
+      end
+    end
+  end
+
+  def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    user_input = gets.strip
+
+    if genre = Genre.find_by_name(user_input)
+      genre.songs.sort{ |instance1, instance2| instance1.name <=> instance2.name }.each.with_index(1) do |instance, index|
+        puts "#{index}. #{instance.artist.name} - #{instance.name}"
+      end
+    end
+  end
+
+  def play_song
+    puts "Which song number would you like to play?"
+    user_input = gets.strip.to_i
+
+    if song = Song.all.sort{ |instance1, instance2| instance1.name <=> instance2.name }[user_input - 1]
+      # binding.pry
+      if user_input > 1 && user_input <= Song.all.length
+        puts "Playing #{song.name} by #{song.artist.name}"
       end
     end
   end
