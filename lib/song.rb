e0acc6af341fa233c416b8defa4883cff4d4582a
arielord <arielord@buffalo.edu>
2018-12-30 03:57:17 -0500

    Song, Artist, and Genre basics.

---
+++ lib/song.rb
@@ -0,0 +1,26 @@
+class Song
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    self.name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    song = Song.new(name)
+    @@all << song
+    song
+  end
+end
