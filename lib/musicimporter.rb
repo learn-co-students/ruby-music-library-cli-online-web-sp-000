4cf3c3abfd737320f1bdf81c60cf7110cb5edeed
Meisuleen Kased <meisuleen@gmail.com>
2019-01-15 16:34:09 +0000

    Done.

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -1,17 +1,21 @@
 require 'pry'
 class MusicImporter
-
+  attr_accessor :path
   def initialize(file_path)
     @path = file_path
   end
 
   def path
-    # Dir["./spec/fixtures/mp3s"].select {|f| File.file? f}
-     Dir["./spec/fixtures/mp3s"]
+     Dir["./spec/fixtures/mp3s"].select {|f| File.file? f}
+     @path
   end
 
   def files
-
+    current_dir = Dir.getwd
+    Dir.chdir(@path)
+    filenames = Dir.glob("*.mp3")
+    Dir.chdir(current_dir)
+    filenames
   end
 
 end
