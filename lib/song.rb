cf375aec8b2b1c8d7e6f1c4f819477cd562ff547
Diana Badas <diana.badas@gmail.com>
2019-02-01 16:01:28 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -53,4 +53,16 @@ class Song
     @genre
   end
 
+  def self.find_by_name(name)
+    self.all.detect { |s| s.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if find_by_name(name)
+      find_by_name(name)
+    else
+      create(name)
+    end
+  end
+
 end
