1be759213275a948958f344d37384aa73ea6eb43
Diana Badas <diana.badas@gmail.com>
2019-02-01 13:01:08 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,16 +1,13 @@
 class Artist
 
   attr_accessor :name
+  attr_reader :songs
 
   @@all = []
 
   def initialize(name)
     @name = name
     @songs = []
-  end
-
-  def songs
-    @songs
   end
 
   def self.all
