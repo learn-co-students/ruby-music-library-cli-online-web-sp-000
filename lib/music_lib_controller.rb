0b34765a4c105f1f588ce72dfc8c9847b44d756a
John Hayes <johnnyhayes7@gmail.com>
2019-02-20 14:39:45 +0000

    Done.

--- lib/music_lib_controller.rb
+++ lib/music_lib_controller.rb
@@ -23,10 +23,26 @@ class MusicLibraryController
     
     input = gets.strip
     
+    case input 
+      when "list songs"
+        self.list_songs
+      when "list artists"
+        self.list_artists
+      when "list genres"
+        self.list_genres
+      when "list artist"
+        self.list_songs_by_artist
+      when "list genre"
+        self.list_songs_by_genre
+      when "play song"
+        self.play_song
+    end
+    
     until input == "exit"
       puts "What would you like to do?"
       input = gets.strip
     end
+    
   end
   
   
@@ -65,15 +81,49 @@ class MusicLibraryController
   end
   
   
-  #THIS IS WHERE I LEFT OFF...EVERYTHING UNDER "PUTS" DOESN'T WORK
   def list_songs_by_artist 
     puts "Please enter the name of an artist:"
     
     input = gets.strip
     
-    x = Artist.all.find(input)
-    binding.pry
+    x = Artist.find_by_name(input)
     
+    if x != nil
+      sorted_array = x.songs.sort_by{|s| s.name}
+      sorted_array.each_with_index do |s, i|
+        puts "#{i+1}. #{s.name} - #{s.genre.name}"
+      end
+    end
+  end
+  
+  def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    
+    input = gets.strip
+    
+    x = Genre.find_by_name(input)
+    
+    if x != nil
+      sorted_array = x.songs.sort_by{|s| s.name}
+      sorted_array.each_with_index do |s, i|
+        puts "#{i+1}. #{s.artist.name} - #{s.name}"
+      end
+    end
+  end
+  
+  def play_song
+    puts "Which song number would you like to play?"
+    
+    input = gets.strip.to_i
+    
+    song_array = Song.all.collect {|s| s}
+    
+    song_array = song_array.uniq
+    sorted_array = song_array.sort_by{|s| s.name}
+    
+    if input > 0 && input <= sorted_array.length
+      puts "Playing #{sorted_array[input-1].name} by #{sorted_array[input-1].artist.name}"
+    end
   end
   
 end
