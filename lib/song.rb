1f94e5a18a81d8fcdedae9c5004e26ff85a5c616
ADIL NAIMI <adilnm23@hotmail.com>
2019-04-01 02:41:57 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -43,9 +43,21 @@ class Song
     song
   end
   
+  def self.find_by_name(name)
+    @@all.detect{|song| song.name==name}
+  end
+  
+  def self.find_or_create_by_name(name)
+     if(self.find_by_name(name))
+       self.find_by_name(name)
+     else 
+       self.create(name)
+     end
+  end
   
 end
 
 #learn spec/005_songs_and_genres_spec.rb
 #learn spec/004_songs_and_artists_spec.rb
 #learn spec/006_artists_and_genres_spec.rb
+#learn spec/007_findable_songs_spec.rb
