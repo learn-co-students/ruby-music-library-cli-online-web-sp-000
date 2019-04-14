41448d32fe83279c321e1838d802270682ba2441
Stuart Hahn <stuart.a.hahn@gmail.com>
2019-04-14 19:26:19 +0000

    Done.

---
+++ lib/music_importer.rb
@@ -0,0 +1,16 @@
+require 'pry'
+class MusicImporter
+
+  attr_accessor :path
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    Dir.entries(path).select {|f| !File.directory? f}
+  end
+
+  def import
+    files.each {|f| Song.create_from_filename(f)}
+  end
+end
