0dc2471b94f0eff99df8ead876c7529a6c3b4253
Sandesh Karki <karkisandesh@hotmail.com>
2019-05-14 20:42:07 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class MusicLibraryController
   
   attr_reader :path
@@ -7,6 +9,8 @@ class MusicLibraryController
   end
   
   def call 
+    input = ''
+    while input != "exit"
       puts "Welcome to your music library!"
       puts "To list all of your songs, enter 'list songs'."
       puts "To list all of the artists in your library, enter 'list artists'."
@@ -17,7 +21,60 @@ class MusicLibraryController
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
       
-      
+      input = gets.strip
+    end
   end
+  
+  def list_songs
+    Song.all.sort_by(&:name).each.with_index(1) do |song, index|
+      puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+    end
+  end
+  
+   def list_artists
+    Artist.all.sort_by(&:name).each.with_index(1) do |artist, index|
+      puts "#{index}. #{artist.name}"
+    end
+  end
+  
+  def list_genres
+    Genre.all.sort_by(&:name).each.with_index(1) do |genre, index|
+      puts "#{index}. #{genre.name}"
+    end
+  end
+  
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    input = gets.strip
+    
+    if artist = Artist.find_by_name(input)
+       artist.songs.sort_by(&:name).each.with_index(1) do |song, index|
+        
+      puts "#{index}. #{song.name} - #{song.genre.name}"
+      end
+    end
+  end
+  
+   def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    input = gets.strip
+    
+    if genre = Genre.find_by_name(input)
+       genre.songs.sort_by(&:name).each.with_index(1) do |song, index|
+        
+      puts "#{index}. #{song.artist.name} - #{song.name}"
+      end
+    end
+  end
+  
+  def play_song
+    puts "Which song number would you like to play?"
+      input = gets.strip.to_i
+      if (1..Song.all.length).include?(input)
+      song = Song.all.sort_by(&:name)[input-1]
+      puts "Playing #{song.name} by #{song.artist.name}"
+    end
+  end
+
 
 end
