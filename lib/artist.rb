22f4801814f63e70740187d6eec3fe0ff6884a14
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-05-30 01:16:28 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -34,9 +34,10 @@ class Artist
   end
   
   def add_song(song)
-    @songs << song
-    song.artist = self
-    if @artist 
-      artist.name
+    if @songs.include?(song)
+      @artist.name
+    else 
+      @songs << song
+    end
   end
 end
