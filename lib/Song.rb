702596a03942f0042ffd34fa2c30814be47dfb9d
Andrew Silva <andrewsilva9@gmail.com>
2019-06-03 01:31:52 +0000

    Done.

---
+++ lib/Song.rb
@@ -0,0 +1,29 @@
+class Song
+
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+   self.new(name).tap do|song|
+     song.save
+   end
+ end
+
+end
