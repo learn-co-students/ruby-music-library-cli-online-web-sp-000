563ebc7109ae4acd0e1c81a2e2f6ae7ef20e4637
Alan Krajina <alankrajina88@gmail.com>
2019-06-05 13:30:00 +0000

    Done.

---
+++ lib/concerns/artist.rb
@@ -0,0 +1,34 @@
+class Artist
+  attr_accessor :name, :songs
+  
+  def initialize(name)
+    @name = name
+    @@all = []
+    @songs = []
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.destroy_all
+    @@all = []
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.create(new_artist)
+    artist = Artist.new(new_artist)
+    artist.save
+    artist
+  end
+  
+  def add_song(song)
+    if song.artist != self
+    song.artist = self
+    @songs << song
+  end
+  end
+end
---
+++ lib/concerns/song.rb
@@ -0,0 +1,49 @@
+class Song
+  attr_accessor :name, :song
+  attr_reader :artist
+  
+  def initialize(name,artist = "nil")
+    @name = name
+    @@all = []
+    @artist = artist
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.destroy_all
+    @@all = []
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.create(new_song)
+    song = Song.new(new_song)
+    song.save
+    song
+  end
+
+
+  def artist=(artist_object)
+    Artist.add_song(artist_object)
+  end
+end
+
+# dont know how to invoke Artist add_song method inside artist= method
+
+#mora dodat artist_object
+
+#    artist.song = self
+#   @songs << artist
+ 
+# Failures:
+
+#  1) Associations — Song and Artist: Song #artist= invokes Artist#add_song to add itself to the artist's collection of songs (artist has many songs)
+#     Failure/Error: expect(artist).to receive(:add_song)
+
+#       (#<Artist:0x0000000002ffb9d0 @name="Neutral Milk Hotel", @songs=[]>).add_song(*(any args))
+#           expected: 1 time with any arguments
+#           received: 0 times with any arguments
