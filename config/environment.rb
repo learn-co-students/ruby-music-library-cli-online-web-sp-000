f1d3114fd82b1592dd8e9e9fe8d0c95df8c2bf91
Andrew Silva <andrewsilva9@gmail.com>
2019-06-09 22:03:30 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,11 @@ require 'bundler'
 Bundler.require
 
 module Concerns
+  module Findable
+    def find_by_name(name)
+      self.all.detect { |object| object.name == name }
+    end
+  end
 end
 
 require_all 'lib'
--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,3 +1,1 @@
-module Concerns::Findable
 
-end
