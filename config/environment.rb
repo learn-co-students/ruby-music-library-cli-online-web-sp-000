57dd5524f2c5b21f8df8c6350fd7b5ef0ca28ecf
Andrew Silva <andrewsilva9@gmail.com>
2019-06-09 22:13:57 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -6,6 +6,15 @@ module Concerns
     def find_by_name(name)
       self.all.detect { |object| object.name == name }
     end
+
+    def find_or_create_by_name(name)
+      song = self.find_by_name(name)
+      if song
+        song
+      else
+        self.create(name)
+      end
+    end
   end
 end
 
