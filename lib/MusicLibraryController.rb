e288f11178d85c5bfce7196ab18d0a5af4e42397
Andrew Silva <andrewsilva9@gmail.com>
2019-06-12 01:01:13 +0000

    Done.

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -10,9 +10,8 @@ class MusicLibraryController
     input = gets.strip
 
     if input != exit
-    puts "Welcome to your music library!"
-    puts "To list all of your songs, enter 'list songs'."
-    end
+      puts "Welcome"
+    end   
   end
 
 end
