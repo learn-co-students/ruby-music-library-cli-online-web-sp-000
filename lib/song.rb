4a5b0c65b841d9aa0524019f6da8babc5fd15822
Tom White <eugene.white@gmail.com>
2019-06-22 17:05:29 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -48,4 +48,8 @@ class Song
     @@all.find{ |song| song.name == name}
   end
     
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+  
 end
