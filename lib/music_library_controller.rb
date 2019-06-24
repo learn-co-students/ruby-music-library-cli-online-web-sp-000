da0df0913bd64f89e9108c1888609d67b978dc86
Tom White <eugene.white@gmail.com>
2019-06-24 05:39:56 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -19,12 +19,20 @@ class MusicLibraryController
     answer = ""
     while answer != "exit"
       puts "What would you like to do?"
-      answer = gets.chomp
+      answer = gets.chomp.downcase
       case answer
       when "list songs"
         list_songs
       when "list artists"
         list_artists
+      when "list genres"
+        list_genres
+      when "list artist"
+        list_songs_by_artist
+      when "list genre"
+        list_songs_by_genre
+      when "play song"
+        play_song
       end
     end
   end
@@ -40,6 +48,40 @@ class MusicLibraryController
     artists.each.with_index(1) {|artist, i| puts "#{i}. #{artist.name}"}
   end
 
+  def list_genres
+    genres = Genre.all.sort {|a,b| a.name <=> b.name}
+    genres.each.with_index(1) {|genre, i| puts "#{i}. #{genre.name}"}
+  end
+  
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    input = gets.strip
+    if artist = Artist.find_by_name(input)
+      artist.songs.sort {|a,b| a.name <=> b.name}.each.with_index(1) do |song, index|
+      puts "#{index}. #{song.name} - #{song.genre.name}"
+      end
+    end
+  end 
+  
+  def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    input = gets.strip
+    if genre = Genre.find_by_name(input)
+      genre.songs.sort {|a,b| a.name <=> b.name}.each.with_index(1) do |song, index|
+      puts "#{index}. #{song.artist.name} - #{song.name}"
+      end
+    end
+  end 
       
+  def play_song
+    puts "Which song number would you like to play?"
+    input = gets.chomp.to_i
+    songs = Song.all
+    if (1..songs.length).include?(input)
+      song = Song.all.sort{ |a, b| a.name <=> b.name }[input - 1]
+    end
+    puts "Playing #{song.name} by #{song.artist.name}" if song
+  end
+
   
 end
