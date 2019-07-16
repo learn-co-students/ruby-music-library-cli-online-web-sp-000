09992f4fa05dd7bb25e9dbe9d79405228bdd6ff3
Connor Cowan <ccowan003@comcast.net>
2019-07-16 22:15:35 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,20 @@
+class Song
+  attr_accessor :name, :artist
+  @@ all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    return @@all
+  end
+
+  def self.destroy_all
+    return @@all.clear
+  end
+
+
+
+end
