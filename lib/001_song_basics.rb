6a765227982edf0f35c55dbeed144beaf4278080
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-18 22:20:17 +0000

    Done.

--- lib/001_song_basics.rb
+++ lib/001_song_basics.rb
@@ -1,15 +1,23 @@
+
+require 'pry'
 class Song
-attr_accessor :name, :genre, :artist
+attr_accessor :name, :genre
+attr_reader :artist
 @@all = []
 
 def initialize(name, artist = nil)
   @name = name
-  @artist = artist
+  # @artist = artist
+  self.artist= artist if artist != nil
 end
 
 def artist=(artist)
   @artist = artist
-  @artist.add_song(self)
+  artist.add_song(self)
+  # binding.pry
+end
+def artist
+  @artist
 end
 
 def self.create(name)
