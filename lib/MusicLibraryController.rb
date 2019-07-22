c68e1bda66d8d6d42964ed047b734ed5d91c12d3
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-22 18:19:09 +0000

    Done.

---
+++ lib/MusicLibraryController.rb
@@ -0,0 +1,43 @@
+require 'pry'
+class MusicLibraryController
+  attr_accessor :path
+
+ def initialize(path = './db/mp3s')
+   @path = path
+   new_files = MusicImporter.new(path)
+   new_files.import
+ end
+def call
+  input = ""
+until input == "exit" do
+puts "Welcome to your music library!"
+puts "To list all of your songs, enter 'list songs'."
+puts "To list all of the artists in your library, enter 'list artists'."
+puts "To list all of the genres in your library, enter 'list genres'."
+puts "To list all of the songs by a particular artist, enter 'list artist'."
+puts "To list all of the songs of a particular genre, enter 'list genre'."
+puts "To play a song, enter 'play song'."
+puts "To quit, type 'exit'."
+puts "What would you like to do?"
+input = gets.strip
+end
+
+def list_songs
+  Song.all.sort {|a,b| a.name <=> b.name}.each.with_index(1) do |song, i|
+      puts "#{i}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+      # binding.pry
+   end
+ end
+end
+def list_artists
+  artists = Artist.all.sort {|a,b| a.name <=> b.name}
+      artists.each.with_index(1) {|artist, i| puts "#{i}. #{artist.name}"}
+  end
+def list_genres
+  genres = Genre.all.sort {|a,b| a.name <=> b.name}
+  genres.each.with_index(1) {|genre, i| puts "#{i}. #{genre.name}"}
+end
+
+
+
+end
