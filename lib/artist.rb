bd7ed05f0be91f7c87d1eaa5164ccbbbf342bd3b
Lindsey Wells <lindsey.wells84@gmail.com>
2019-07-25 05:06:43 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,30 @@
+class Artist
+
+attr_accessor :name
+
+@@all = []
+
+  def initialize(name)
+  @name=name
+  @@all << self
+  end
+  
+   def self.all
+    @@all
+  end
+  
+  def self.destroy_all
+    @@all.clear
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.create(name)
+   artist = Artist.new(name)
+   artist.save
+   artist
+end
+  
+end
