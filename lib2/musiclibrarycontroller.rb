61c52fd8cddffbc2331de3edd8d00328e23a1040
David Strauch <dmaster18@gmail.com>
2019-09-30 15:44:25 +0000

    Done.

--- lib2/musiclibrarycontroller.rb
+++ lib2/musiclibrarycontroller.rb
@@ -1,44 +1,59 @@
 class MusicLibraryController
 
   extend Concerns::Findable
-
   
-  def initialize(user_input = './db/mp3s')
-    @user_input = user_input
-    music_importer = MusicImporter.new(user_input)
-    music_importer.import
+  def initialize(path = './db/mp3s')
+    @path = path
+    music_importer = MusicImporter.new(@path)
+    @songs = music_importer.import
   end  
 
   def call
     puts "Welcome to your music library!"
-    exit_value = ""  
-    
+    puts "To list all of your songs, enter 'list songs'."
+    puts "To list all of the artists in your library, enter 'list artists'."
+    puts "To list all of the genres in your library, enter 'list genres'."
+    puts "To list all of the songs by a particular artist, enter 'list artist'."
+    puts"To list all of the songs of a particular genre, enter 'list genre'."
+    puts "To play a song, enter 'play song'."
+    puts "To quit, type 'exit'."
+    puts "What would you like to do?"
+    exit_value = ""
     while exit_value != "exit"
-      
-      if exit_value != "exit"
-        puts "To list all of your songs, enter 'list songs'."
-        @user_input = gets.chomp
-        
-        if @user_input == 'list songs'
-          list_songs
-        end
-        
+      user_input = gets.chomp
+      if user_input == "list songs"
+        list_songs
+      elsif user_input == "list artists"
+        list_artists
+      elsif user_input == "list genres"
+        list_genres
+      elsif user_input == "list artist"
+        list_artist
+      elsif user_input == "list genre"
+        list_genre
+      elsif user_input == "play song"
+        play_song
+      elsif user_input == "exit"
+        exit_value = user_input
       end
-      
-      puts "If you would like to exit, please type in exit"
-      exit_value = gets.chomp
-    
     end
-  
   end
   
   def list_songs
-    puts Song.all.sort
+  end
+  
+  def list_artists
+  end
+  
+  def list_genres
+  end
+  
+  def list_artist
+  end
+  
+  def list_genre
   end
   
   def play_song
-    
   end
-
-  
 end
