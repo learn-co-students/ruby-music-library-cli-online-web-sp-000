c7e4792dd6163833f0adf57b5fbaeff7a2d0579c
Chantel Lowe <chantellowe17@gmail.com>
2019-10-03 01:20:37 +0000

    Done.

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,5 +1,6 @@
 class Genre 
   attr_accessor :name 
+  extend Concerns::Findable
   
   @@all = []
   
--- lib/song.rb
+++ lib/song.rb
@@ -47,5 +47,23 @@ class Song
     self.find_by_name(name) || self.create(name)
   end
   
+  def self.new_from_filename(filename)
+    new_filename = filename.chomp(".mp3").split(" - ")
+    
+    artist_name = new_filename[0]
+    song_name = new_filename[1]
+    genre_name = new_filename[2]
+    
+    artist = Artist.find_or_create_by_name(artist_name)
+    genre = Genre.find_or_create_by_name(genre_name)
+    
+    self.new(song_name, artist, genre)
+    
+  end
+  
+  def self.create_from_filename(filename)
+    self.new_from_filename(filename).save
+  end
+  
   
 end
