d5f975ac40be7af46694f40c5bccf95be3dedb07
Chantel Lowe <chantellowe17@gmail.com>
2019-10-05 15:49:07 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -49,4 +49,15 @@ class MusicLibraryController
     end
   end
   
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    input = gets.chomp
+    
+    if artist = Artist.find_by_name(input)
+      artist.songs.sort{|a,b| a.name <=> b.name}.each_with_index do |song, i|
+        puts "#{i+1}. #{song.name} - #{song.genre.name}"
+      end
+    end
+  end
+  
 end
