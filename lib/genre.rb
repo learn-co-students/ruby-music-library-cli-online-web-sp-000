eaaececf783e93ca4866ca1758f9862dd1873e33
Anthony Tilelli <anthony@tilelli.me>
2019-10-27 22:39:02 +0000

    Done.

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,2 +1,24 @@
-class Genre < Base
+class Genre
+  @@all = []
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.destroy_all
+    @@all.clear
+  end 
+  
+  def save
+    @@all << self
+  end
+  
+  def self.create(name)
+    new(name).tap{ |g| g.save }
+  end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,2 +1,24 @@
-class Song < Base
+class Song
+  @@all = []
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.destroy_all
+    @@all.clear
+  end 
+  
+  def save
+    @@all << self
+  end
+  
+  def self.create(name)
+    new(name).tap{ |g| g.save }
+  end
 end
