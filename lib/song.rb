ef4d27d213299b364bd63e981b88e41d52d70a85
Anthony Tilelli <anthony@tilelli.me>
2019-10-27 23:36:32 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -40,10 +40,12 @@ class Song
   end
   
   ## ---
-  def find_by_name(name)
+  def self.find_by_name(name)
+    self.all.find {|song| song.name == name }
   end
   
-  def find_or_create_by_name(name)
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create(name)
   end
   ## ---
   
