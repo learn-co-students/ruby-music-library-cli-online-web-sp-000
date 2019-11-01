9f676a68bf4019df342dfc3db1e7c3f89b73c18e
Ronda D Cowen <cowenhome5@gmail.com>
2019-11-01 16:31:54 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,26 @@
+class Song
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  extend Findable::ClassMethods
+  include Paramable::InstanceMethods
+  attr_accessor :name
+  attr_reader :artist
+
+  @@songs = []
+
+  def initialize
+    super
+  end
+
+  def self.all
+    @@songs
+  end
+
+  def artist=(artist)
+    @artist = artist
+  end
+
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
+end
