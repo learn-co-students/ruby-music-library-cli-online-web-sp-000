4f915882baaa6dc5bb77fd3ee677c5d2a4c6dee4
Michael Carroll <carrollm2@gmail.com>
2019-11-14 22:56:30 +0000

    Done.

---
+++ lib/Artist.rb
@@ -0,0 +1,29 @@
+class Artist
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    new_artist = Artist.new(name)
+    new_artist.save
+    new_artist
+  end
+
+end
