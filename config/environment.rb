6720cf3d3997e436b3e1691c6dad76e770b62a17
Michael Carroll <carrollm2@gmail.com>
2019-11-15 23:24:30 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,27 @@ require 'bundler'
 Bundler.require
 
 module Concerns
+  module Findable
+    module ClassMethods
+      def find_by_name(name)
+        objects = []
+        ObjectSpace.each_object(self){|obj| objects << obj}
+        objects.reverse.detect{|a| a.name == name}
+      end
+    end
+  end
+end
+
+class Song
+  extend Concerns::Findable::ClassMethods
+end
+
+class Artist
+  extend Concerns::Findable::ClassMethods
+end
+
+class Genre
+  extend Concerns::Findable::ClassMethods
 end
 
 require_all 'lib'
