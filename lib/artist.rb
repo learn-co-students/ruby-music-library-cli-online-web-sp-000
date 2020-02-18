07099eac5df529dc13662d9678e4bf81d7be5c1f
Chloe Gell <gellert.chloe@gmail.com>
2020-02-18 01:19:43 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,44 +1,45 @@
 require_relative './concerns/Findable.rb'
 
 class Artist
-  attr_accessor :name, :songs, :genres
-  @@all = []
   extend Concerns::Findable
+
+  attr_accessor :name, :songs, :genres
+
+  @@all = []
+
   def initialize(name)
     @name = name
     @songs = []
-    @genres = []
   end
+
   def self.all
     @@all
   end
+
   def save
     @@all << self
   end
+
   def self.destroy_all
     self.all.clear
   end
+
   def self.create(name)
     new_artist = Artist.new(name)
     new_artist.save
     new_artist
   end
+
   def add_song(song)
     if !(song.artist)
       song.artist = self
-      self.songs << song
-      if(song.genre)
-        self.genres << song.genre
+      if !(self.songs.include?(song))
+        self.songs << song
       end
     end
   end
+
   def genres
-    artist_genres = []
-    songs = Song.all.select {|song| song.artist == self}
-    songs.each do |song|
-      artist_genres.push(song.genre)
-    end
-    artist_genres
-    binding.pry
+    songs.map {|song| song.genre}
   end
 end
