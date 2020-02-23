093a49016fe9dde33a9640248c66d2b229260c11
Deirdre  Sullivan <dsulliv4@binghamton.edu>
2020-02-23 20:21:53 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,41 +1,41 @@
 class Artist
- extend Concerns::Findable 
+  extend Concerns::Findable
 
-attr_accessor :name
-attr_reader :songs
-@@all = []
+  attr_accessor :name
+  attr_reader :songs
 
-def initialize (name)
-  @name = name
-  @songs = []
-  save
-end
+  @@all = []
 
-def self.all
-  @@all
-end
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
 
-def save
-  self.class.all << self
-end
+  def self.all
+    @@all
+  end
 
-def self.destroy_all
-  all.clear
-end
+  def self.destroy_all
+    all.clear
+  end
 
-def self.create(name)
-  artist = Artist.new(name)
-  artist.save
-  artist
-end
+  def save
+    self.class.all << self
+  end
 
-def add_song(song)
+  def self.create(name)
+    artist = new(name)
+    artist.save
+    artist
+
+  end
+
+  def add_song(song)
     song.artist = self unless song.artist
     songs << song unless songs.include?(song)
-end
+  end
 
-def genres
-  songs.collect{|s| s.genre}.uniq
-end
-
+  def genres
+    songs.collect{ |s| s.genre }.uniq
+  end
 end
