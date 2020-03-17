9302696d0c25917d8459faedcb5e0c531647c2de
Xiaoyi Liu <lxy878@gmail.com>
2020-03-17 10:36:34 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -16,6 +16,21 @@ class MusicLibraryController
     puts "What would you like to do?"
     command = gets.chomp
     until command == 'exit'
+      case command
+      when 'list songs'
+        self.list_songs
+      when 'list artists'
+        self.list_artists
+      when 'list genres'
+        self.list_genres
+      when 'list artist'
+        self.list_songs_by_artist
+      when 'list genre'
+        self.list_songs_by_genre
+      when 'play song'
+        self.play_song
+      else
+      end
       command = gets.chomp
     end
 
