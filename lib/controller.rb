2ea90538a1005f6c8e68a3bbd39c1d0e9be411e4
Ryan Mui <reachryanmui@gmail.com>
2020-04-03 02:23:01 +0000

    Done.

--- lib/controller.rb
+++ lib/controller.rb
@@ -33,4 +33,37 @@ class MusicLibraryController
       puts "#{i+1}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
     end
   end
+
+  def list_artists
+    sortedArtists = Artist.all.sort! {|a,b| a.name <=> b.name}
+    sortedArtists.each_with_index do |a, i|
+      puts "#{i+1}. #{a.name}"
+    end
+  end
+
+  def list_genres
+    sortedGenres = Genre.all.sort! {|a,b| a.name <=> b.name}
+    sortedGenres.each_with_index do |g, i|
+      puts "#{i+1}. #{g.name}"
+    end
+  end
+
+  def list_songs_by_artist
+    songsOfArtist = []
+
+    puts "Please enter the name of an artist:"
+    target = gets.strip
+    Song.all.each do |s|
+      if s.artist.name == target
+        songsOfArtist << s
+      end
+    end
+
+    sortedSongs = songsOfArtist.sort! {|a,b| a.name <=> b.name}
+    # alphabetized = []
+    sortedSongs.each_with_index do |s, i|
+      # puts alphabetized << "#{i+1}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
+      puts "#{i+1}. #{s.name} - #{s.genre.name}"
+    end
+  end
 end
