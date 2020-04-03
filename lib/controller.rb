4c9f74306d76aeb40acb1cceff705b17e23836ba
Ryan Mui <reachryanmui@gmail.com>
2020-04-03 03:13:27 +0000

    Done.

--- lib/controller.rb
+++ lib/controller.rb
@@ -66,4 +66,33 @@ class MusicLibraryController
       puts "#{i+1}. #{s.name} - #{s.genre.name}"
     end
   end
+
+  def list_songs_by_genre
+    songsOfGenre = []
+
+    puts "Please enter the name of a genre:"
+    target = gets.strip
+    Song.all.each do |s|
+      if s.genre.name == target
+        songsOfGenre << s
+      end
+    end
+
+    sortedSongs = songsOfGenre.sort! {|a,b| a.name <=> b.name}
+    # alphabetized = []
+    sortedSongs.each_with_index do |s, i|
+      # puts alphabetized << "#{i+1}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
+      puts "#{i+1}. #{s.artist.name} - #{s.name}"
+    end
+  end
+
+  def play_song
+    puts "Which song number would you like to play?"
+    target = gets.strip
+    sortedSongs = Song.all.sort! {|a,b| a.name <=> b.name}
+    # binding.pry
+    p = sortedSongs[target]
+    # puts "#{i+1}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
+    puts "Playing #{p.name} by #{p.artist.name}"
+  end
 end
