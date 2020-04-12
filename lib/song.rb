77af953947ff17682e141f1db1daa7c3855824a7
C. Leow <chienleow1@gmail.com>
2020-04-12 23:41:32 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,31 @@
+class Song
+  
+  @@all = []
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+  
+  def save
+    @@all << self
+  end
+
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+  
+  
+end
