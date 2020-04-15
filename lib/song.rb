62eda7ae5fa2eb3341a09fc8a8ff58ae4eb91c05
C. Leow <chienleow1@gmail.com>
2020-04-15 02:55:29 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -46,5 +46,12 @@ class Song
     song
   end
   
+  def self.find_by_name(name)
+    self.all.find {|song| song.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    find_by_name(name) || create(name)
+  end
   
 end
