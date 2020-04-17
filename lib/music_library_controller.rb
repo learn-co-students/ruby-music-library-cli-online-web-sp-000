f51f9435208d8d3336ac771ce1d26b742b2aebcb
C. Leow <chienleow1@gmail.com>
2020-04-17 16:25:04 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -19,6 +19,9 @@ class MusicLibraryController
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
       input = gets.strip
+      if input == 'list songs'
+        list_songs
+      end
     end
   end
 
