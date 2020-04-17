ab565c3e49ab08461a2809f4b03397573db3c303
C. Leow <chienleow1@gmail.com>
2020-04-17 14:42:34 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -61,6 +61,13 @@ class MusicLibraryController
   end
 
   def play_song
+    puts "Which song number would you like to play?"
+    input = gets.strip
+    if genre = Genre.find_by_name(input)
+      genre.songs.sort{|a, b| a.name <=> b.name}.each.with_index(1) do |song, index|
+        puts "#{index}. #{song.artist.name} - #{song.name}"
+      end
+    end
   end
 
 end
