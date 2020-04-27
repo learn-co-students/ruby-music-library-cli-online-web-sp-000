08aca8daac4042ce0db1a3e040963268c641ea02
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-27 20:52:23 +0000

    Done.

---
+++ bin/song.rb
@@ -0,0 +1,28 @@
+class Song
+  
+  @@all = []
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.destroy_all
+    self.all.clear
+  end
+  
+  def self.create(name)
+    song = self.new(name)
+    save
+  end
+  
+end
