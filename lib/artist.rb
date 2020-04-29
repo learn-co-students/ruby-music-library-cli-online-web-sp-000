a119997cfcacde500565ea47d888ce570c96bad1
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-29 05:41:10 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,12 @@
 class Artist
   
   @@all = []
-  
-  attr_accessor :name
+  extend Concerns::Findable
+  attr_accessor :name, :songs
   
   def initialize(name)
     @name = name
+    @songs = []
   end
   
   def self.all
@@ -16,7 +17,27 @@ class Artist
     @@all << self
   end
   
-  def self.destroy
+  def songs
+    @songs
+  end
+  
+  def genres
+    self.songs.collect {|s| s.genre}.uniq
+  end
+  
+  def add_song(song)
+    song.artist = self unless song.artist == self
+    @songs << song unless @songs.include?(song)
+  end
+  
+  def self.destroy_all
     self.all.clear
   end
+  
+  def self.create(name)
+    artist = self.new(name)
+    artist.save
+    artist
+  end
+  
 end
--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,20 @@ class Song
   
   @@all = []
   
-  attr_accessor :name
+  attr_accessor :name, :artist, :genre
   
-  def initialize(name)
+  def initialize(name, artist = nil, genre = nil)
     @name = name
+    self.artist = artist if artist
+    self.genre = genre if genre
+  end
+  
+  def self.find_by_name(name)
+    self.all.detect{|s| s.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create(name)
   end
   
   def self.all
@@ -16,13 +26,36 @@ class Song
     @@all << self
   end
   
+  def self.new_from_filename(filename)
+    parts = filename.split(" - ")
+    title = parts[1]
+    artist = Artist.find_or_create_by_name(parts[0])
+    genre = Genre.find_or_create_by_name(parts[2].chomp(".mp3")) 
+    new_song = Song.new(title, artist, genre)
+  end
+  
+  def self.create_from_filename(filename)
+    self.all << self.new_from_filename(filename)
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+  
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self unless genre.songs.include?(self)
+    
+  end
+
   def self.destroy_all
     self.all.clear
   end
   
-  def self.create(name)
+  def self.create(name, artist = nil, genre = nil)
     song = self.new(name)
-    save
+    song.save
     song
   end
   
