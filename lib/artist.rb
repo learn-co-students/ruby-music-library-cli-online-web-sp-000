67e607be8bf158d533fc7bcd5faea2dc46ab827b
Arodriguez1030 <arodriguez1030@gmail.com>
2020-05-30 09:24:05 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,41 @@
+class Artist
+    attr_accessor :name, :song
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @songs = []
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.destroy_all
+        @@all.clear
+    end
+
+    def self.create(artist_name)
+        new_artist = Artist.new(artist_name)
+        @@all << new_artist
+        new_artist
+    end
+
+    def songs
+        @songs
+    end
+
+    def add_song(song)
+        if song.artist == nil
+            song.artist = self
+        end
+        unless songs.include?(song)
+        @songs << song
+        end
+    end
+end
