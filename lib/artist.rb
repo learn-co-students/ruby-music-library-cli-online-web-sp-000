1da8b846ba88f386170827efef2c0af5b9f0f432
Arodriguez1030 <arodriguez1030@gmail.com>
2020-06-02 21:49:43 -0400

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,6 @@
 class Artist
     attr_accessor :name, :song
+    extend Concerns::Findable
 
     @@all = []
 
--- lib/song.rb
+++ lib/song.rb
@@ -49,4 +49,16 @@ class Song
     def self.find_or_create_by_name(name)
        find_by_name(name) || create(name)
     end
+
+    def self.new_from_filename(file)
+        artist, song, genre_name = file.split(" - ")
+        fixed_name = genre_name.gsub(".mp3", '')
+        artist = Artist.find_or_create_by_name(artist)
+        genre = Genre.find_or_create_by_name(fixed_name)
+        new(song, artist, genre)
+    end
+
+    def self.create_from_filename(file)
+        new_from_filename(file).save
+    end
 end
