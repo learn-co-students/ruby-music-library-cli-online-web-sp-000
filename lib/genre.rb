a333eaf713b41c4e7e5ff4d4dcc8bf97b0a10103
imperative-genre-4923 <dreama.lin@yale.edu>
2020-07-03 21:02:47 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,42 @@
+class Genre
+
+  @@all = []
+
+  attr_accessor :name, :songs
+
+  extend Concerns::Findable
+
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.create(name)
+    genre = self.new(name)
+    genre.save
+    genre
+  end
+
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def songs
+    @songs
+  end
+
+  def artists
+    self.songs.collect {|song| song.artist}.uniq
+  end
+
+end
