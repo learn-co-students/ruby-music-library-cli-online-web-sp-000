6537c4f9d886cafb9bd12192b2c2aa32a3f49c10
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-07-19 20:11:09 +0000

    Done.

--- lib/site_generator.rb
+++ lib/site_generator.rb
@@ -5,17 +5,19 @@ class SiteGenerator
     @path = path
     FileUtils.mkdir_p path
     FileUtils.mkdir_p "#{path}/artists"
+    FileUtils.mkdir_p "#{path}/genres"
+    FileUtils.mkdir_p "#{path}/songs"
   end
 
-def generate_index
+def generate_index(template_path)
 #  HEREDOC
 # First load the template string
-template_string = File.read("./lib/views/#{template}.erb")
+template_string = File.read("./lib/views/#{template_path}.erb")
 # Instantiate the ERB template instance
 template = ERB.new(template_string)
 html = template.result
 
-File.write("#{path}/index.html", html)
+File.write("#{path}/#{template_path}", html)
 end
 
 def call
@@ -27,9 +29,10 @@ end
 
 end
 
+#template generates what is inside of there^^^
 #index will read the view
 #generate a template
 #compile the template
-#then write it 
+#then write it
 
 #the content of the file in def generate_index
