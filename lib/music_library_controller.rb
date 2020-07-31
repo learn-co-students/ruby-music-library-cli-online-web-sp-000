5785d3e27ad168c175a7a508d46ab029b07f3aba
Ian Mayton <ian.mayton@gmail.com>
2020-07-31 17:14:40 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -24,7 +24,7 @@ class MusicLibraryController
   end
   
   def list_songs
-    sorted = Song.all.sort_by {|song| song.name}
+    sorted = Song.all.sort_by{|song| song.name}
     counter = 1 
     sorted.each do |song|
       puts "#{counter}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
@@ -33,7 +33,7 @@ class MusicLibraryController
   end 
   
   def list_artists 
-    sorted = Artist.all.sort_by {|artist| artist.name}
+    sorted = Artist.all.sort_by{|artist| artist.name}
     counter = 1 
     sorted.each do |artist|
       puts "#{counter}. #{artist.name}"
@@ -42,7 +42,7 @@ class MusicLibraryController
   end 
   
   def list_genres
-    sorted = Genre.all.sort_by {|genre| genre.name}
+    sorted = Genre.all.sort_by{|genre| genre.name}
     counter = 1 
     sorted.each do |genre|
       puts "#{counter}. #{genre.name}"
@@ -53,7 +53,7 @@ class MusicLibraryController
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
     input = gets.chomp
-    sorted = Song.all.sort_by {|song| song.name}
+    sorted = Song.all.sort_by{|song| song.name}
     counter = 1 
     sorted.each do |song|
       if song.artist.name == input
@@ -66,7 +66,7 @@ class MusicLibraryController
   def list_songs_by_genre
     puts "Please enter the name of a genre:"
     input = gets.chomp
-    sorted = Song.all.sort_by {|song| song.name}
+    sorted = Song.all.sort_by{|song| song.name}
     counter = 1 
     sorted.each do |song|
       if song.genre.name == input
@@ -79,7 +79,7 @@ class MusicLibraryController
   def play_song
     puts "Which song number would you like to play?"
     input = gets.to_i
-    sorted = Song.all.sort_by {|song| song.name}
+    sorted = Song.all.sort_by{|song| song.name}
     if (1..sorted.length) === input
       counter = 1 
       selected_song = nil
