19f99f6596e3a1c497b692ac8aa46c82f207ffe2
Alexander Clamor <alexander.clamor@gmail.com>
2020-09-01 04:51:16 +0000

    Done.

--- lib/music_controller.rb
+++ lib/music_controller.rb
@@ -1,3 +1,5 @@
+# NOTE: The Learn IDE was buggy so this code has yet to be tested.
+
 class MusicLibraryController
   attr_accessor :path
 
@@ -10,9 +12,9 @@ class MusicLibraryController
     
     input = ""
 
+    puts "Welcome to your music library!"
+  
     while input != exit
-
-      puts "Welcome to your music library!"
       puts "To list all of your songs, enter 'list songs'."
       puts "To list all of the artists in your library, enter 'list artists'."
       puts "To list all of the genres in your library, enter 'list genres'."
@@ -20,12 +22,25 @@ class MusicLibraryController
       puts "To list all of the songs of a particular genre, enter 'list genre'."
       puts "To play a song, enter 'play song'."
       puts "To quit, type 'exit'."
+      
       puts "What would you like to do?"
-
       input = gets.strip
       
+      case input
+         when "list songs"
+           list_songs
+         when "list artists"
+           list_artists
+         when "list genres"
+           list_genres
+         when "list artist"
+           list_songs_by_artist
+         when "list genre"
+           list_songs_by_genre
+         when "play song"
+           play_song
+       end
     end
-
   end 
 
   def list_songs
@@ -65,6 +80,17 @@ class MusicLibraryController
       genre.songs.sort{|a, b| a.name <=> b.name}.each_with_index do |song, i|
         puts "#{i+1}. #{song.artist.name} - #{song.name}"
       end
+    end
+  end
+  
+  def play_song
+    puts "Which song number would you like to play?"
+    self.list_songs
+    input = gets.strip.to_i
+    if input > 0 && input <= Song.all.length
+      array = Song.all.sort{|a, b| a.name <=> b.name}
+      song = array[input-1]
+      puts "Playing #{song.name} by #{song.artist.name}"
     end
   end
 
