ad6591b6507ec922287bb9b5cbe5d252f57dbfbf
jasonronalddavis <jdavis@c.ringling.edu>
2020-09-24 22:43:11 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -39,10 +39,9 @@ def genre=(genre)
 end
 
 def self.find_by_name(name)
-    @@all.detect do |song|
-        song.name 
+    @@all.find {|song|song.name == name}
   end
-end
+
 
 def self.find_or_create_by_name(name)
 self.find_by_name(name) || self.create(name)
