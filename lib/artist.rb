280db11e7e8b74f842b894d531b3d61afeef0111
Keith Filkins <kafilkins@me.com>
2020-12-30 23:29:24 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,58 @@
+require 'pry'
+
+class Artist
+
+  attr_accessor :name, :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(artist)
+    artist = self.new(artist)
+    artist.save
+    artist
+  end
+
+  def add_song(song)
+    if song.artist == nil
+      song.artist = self
+    else
+      nil
+    end
+    if @songs.include?(song)
+      nil
+    else
+      @songs << song
+    end
+  end
+
+  def genres
+    new_array = []
+    @songs.each do |song|
+      if new_array.include?(song.genre)
+        nil
+      else
+        new_array << song.genre
+      end
+    end
+    new_array
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,60 @@
+require 'pry'
+
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @@all = []
+
+  def initialize(name, artist=nil, genre=nil)
+    @name = name
+    self.artist=(artist) if artist != nil
+    self.genre=(genre) if genre != nil
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(song)
+    song = self.new(song)
+    song.save
+    song
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self unless genre.songs.include?(self)
+  end
+
+  def self.find_by_name(name)
+    @@all.detect do |song|
+      song.name == name
+    end
+  end
+
+  def self.find_or_create_by_name(name)
+    @@all.detect do |song|
+    if song.name == name
+      song
+    else
+      self.create(name)
+    end
+  end
+  end
+  self.find_by_name(name) || self.create(name)
+end
