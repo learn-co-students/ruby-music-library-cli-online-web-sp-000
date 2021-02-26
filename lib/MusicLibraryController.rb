176ebc7767928c0c3ab5632ab88ade3260ffe98b
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2021-02-26 00:27:18 +0000

    Done.

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -12,7 +12,8 @@ class MusicLibraryController
   def call
     puts "Welcome to your music library!"
     input = gets.strip
-    until input != exit
+
+    if input != exit
     puts "To list all of your songs, enter 'list songs'."
     puts "To list all of the artists in your library, enter 'list artists'."
     puts "To list all of the genres in your library, enter 'list genres'."
