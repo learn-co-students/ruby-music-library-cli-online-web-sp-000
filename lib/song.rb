42a3719cad3733baf595a8edd5af3ba276fdc91f
Robert Clarke <r.jason.clarke@gmail.com>
2021-05-06 16:07:21 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,60 @@
+class Song
+
+  attr_accessor :name, :artist, :genre, :musicimporter, :musiclibrarycontroller
+  extend Concerns::Findable
+  @@all = []
+
+  def initialize(name, artist=nil, genre = nil)
+    @name = name
+    self.artist=artist if artist
+    self.genre=genre if genre
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    song = self.new(name)
+    song.save
+    song
+  end
+
+  def artist=(artist)
+    @artist = artist
+    @artist.add_song(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    @genre.songs << self unless @genre.songs.include?(self)
+  end
+
+  def self.find_by_name(name)
+    @@all.detect {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    find_by_name(name) || create(name)
+  end
+
+  def self.new_from_filename(filename)
+    info = filename.split(" - ")
+    artist, name, genre = info[0], info[1], info[2].gsub( ".mp3" , "")
+    genre = Genre.find_or_create_by_name(genre)
+    artist = Artist.find_or_create_by_name(artist)
+    self.new(name,artist,genre)
+  end
+
+  def self.create_from_filename(filename)
+     self.new_from_filename(filename).save
+  end
+end
