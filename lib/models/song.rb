692322e476bf5cf33ff9fdc62adecd7d99bdf0e0
danpaslawsky <danpaslawsky@gmail.com>
2021-05-27 13:55:46 -0700

    Done.

---
+++ lib/models/song.rb
@@ -0,0 +1,20 @@
+class Song
+    attr_accessor :name
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    def intitialize
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.reset_all
+        @@all.clear
+    end
+end
