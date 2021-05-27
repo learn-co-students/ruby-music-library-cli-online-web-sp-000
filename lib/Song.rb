147936d710d8b582868b5ba24594bc226813315c
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-05-27 13:09:22 -0600

    Done.

--- lib/Song.rb
+++ lib/Song.rb
@@ -50,6 +50,7 @@ class Song
         self.new(file_array[1], artist, genre)
 
         # From the solution: 
+        # parts = filename.split(" - ")
         # artist_name, song_name, genre_name = parts[0], parts[1], parts[2].gsub(".mp3", "")
         # artist = Artist.find_or_create_by_name(artist_name)
         # genre = Genre.find_or_create_by_name(genre_name)
