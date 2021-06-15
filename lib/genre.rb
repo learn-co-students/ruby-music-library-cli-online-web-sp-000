640282918a2d21298e785194e2e12e734868a677
danpaslawsky <danpaslawsky@gmail.com>
2021-06-15 13:35:20 -0700

    Done.

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,17 +1,27 @@
 class Genre
-    extend Findable
+    extend Concerns::Findable
     extend Persistable::ClassMethods
     include Persistable::InstanceMethods
 
-    attr_accessor :name, :songs
+    attr_accessor :name
+
     @@all = []
 
     def self.all
         @@all
     end
 
-    def intitialize
-        save
+    def initialize(name)
+        @name = name
     end
+
+    def songs
+        Song.all.select {|s| s.genre == self}
+    end
+
+    def artists
+        songs.collect{ |s| s.artist }.uniq
+    end
+
     
 end
